0f2afa277a720ef0449be3242e76bd8f174a7691
adventista7 <adeleinr@gmail.com>
2010-10-22 14:51:27 -0700

    More Misc changes

---
+++ settings.py
@@ -0,0 +1,14 @@
+# Django settings for webme project.
+from platform import node
+from settings_local import *
+
+print DEVELOPMENT_HOST
+print node()
+
+if node() == DEVELOPMENT_HOST:
+    from settings_development import *
+elif node() == PRODUCTION_HOST:
+    from settings_production import *
+else:
+    raise Exception("Cannot determine execution mode for host '%s'. Please check DEVELOPMENT_HOST and PRODUCTION_HOST in settings_local.py." % node())
+
