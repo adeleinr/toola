263ddba28e7fc9235aeb7e62b04c915e7f134949
adventista7 <adeleinr@gmail.com>
2011-04-03 23:45:59 -0700

    Removed apache-sorl

--- fabfile.py
+++ fabfile.py
@@ -117,7 +117,7 @@ def install_nonpython_requirements():
     sudo('chgrp %s -R %s'% (env.user,env.whole_path))
     
     sudo('cd %s/lib; curl -O http://apache.mirrors.tds.net/lucene/solr/1.3.0/apache-solr-1.3.0.tgz' % (env.whole_path))
-    sudo('cd %s/lib; tar xvzf apache-solr-1.3.0.tgz' % (env.whole_path))
+    #sudo('cd %s/lib; tar xvzf apache-solr-1.3.0.tgz' % (env.whole_path))
     sudo('chown %s -R %s/lib'% (env.user,env.whole_path))
     sudo('chgrp %s -R %s/lib'% (env.user,env.whole_path))
 
